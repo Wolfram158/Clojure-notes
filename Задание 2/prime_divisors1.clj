33229644bd6f6a6bb9840ab00d59257be345d2fa
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-06-06 20:50:02 +0300

    Update prime_divisors1.clj

--- Задание 2/prime_divisors1.clj
+++ Задание 2/prime_divisors1.clj
@@ -1,12 +1,12 @@
 (defn get-divisors-bounded-sqrt [x] 
   (filter 
    #(= (rem x %) 0) 
-   (range 2 (+ 1 (Math/ceil (Math/sqrt x))))))
+   (range 2 (inc (Math/ceil (Math/sqrt x))))))
 
 (defn is-prime [x] 
   (cond (= x 1) false (= x 2) true 
     :else (= 
-           (- (int (Math/floor (Math/sqrt x))) 1) 
+           (dec (int (Math/floor (Math/sqrt x)))) 
            (count (filter #(not= (rem x %) 0) (range 2 (Math/ceil (Math/sqrt x))))))))
 
 (defn get-prime-divisors [x] 
@@ -19,7 +19,7 @@
                                           (reverse (mapv #(/ x %) divisors-bounded-sqrt))))))))
 
 (defn get-power [a b] 
-  (cond (= (rem a b) 0) (+ 1 (get-power (int (Math/floor (/ a b))) b)) :else 0))
+  (cond (= (rem a b) 0) (inc (get-power (int (Math/floor (/ a b))) b)) :else 0))
 
 (defn compact-prime-divisors [x] 
   (let [prime-divisors (get-prime-divisors x)] 
