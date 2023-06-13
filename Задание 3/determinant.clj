0d63adebd844938260c0d2d5ac7354fe4f18ae6f
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-06-13 09:51:55 +0300

    Update determinant.clj

--- Задание 3/determinant.clj
+++ Задание 3/determinant.clj
@@ -11,3 +11,13 @@
                                                                        (Math/pow -1 i) 
                                                                        (get (get matrix 0) i) 
                                                                        (det (subvec (mapv (partial vec-remove i) matrix) 1)))))))
+(defn almost-inverse [matrix] (cond (= 1 (count matrix)) (/
+                                                           1 
+                                                           (first (first matrix))) 
+                                     :else (vec (for [i (range (count matrix))] 
+                                              (vec (for [j (range (count matrix))] (*
+                                                                                    (int (Math/pow -1 (+ i j))) 
+                                                                                    (det (vec-remove i (mapv (partial vec-remove j) matrix))))))))))
+(defn divide-vector [number vect] (mapv #(/ % number) vect))
+(defn divide-matrix [number matrix] (mapv (partial divide-vector number) matrix))                                                                                                                                                                  
+(defn inverse [matrix] (let [determinant (det matrix)] (apply mapv vector ((partial divide-matrix determinant) (almost-inverse matrix)))))
