f2f72040a44d7bfa6608b883f36a31eed06121f0
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-07-10 17:11:54 +0300

    Update matrix_tools.clj
    simplification

--- Задание 3/matrix_tools.clj
+++ Задание 3/matrix_tools.clj
@@ -1,23 +1,17 @@
 (defn vec-remove [pos coll] (into (subvec coll 0 pos) (subvec coll (inc pos))))
-(defn det [matrix] (cond (= 2 (count matrix)) (- 
-                                                 (* 
-                                                 (get (get matrix 0) 0) 
-                                                 (get (get matrix 1) 1)) 
-                                                      (* 
-                                                      (get (get matrix 0) 1) 
-                                                      (get (get matrix 1) 0))) 
-                          :else (apply + 
-                                       (for [i (range (count matrix))] (* 
-                                                                       (Math/pow -1 i) 
-                                                                       (get (get matrix 0) i) 
-                                                                       (det (subvec (mapv (partial vec-remove i) matrix) 1)))))))
+(defn det [matrix] (cond (= 1 (count matrix)) (first (first matrix))
+                         :else (apply +
+                                      (for [i (range (count matrix))] (*
+                                                                        (int (Math/pow -1 i))
+                                                                        (get (get matrix 0) i)
+                                                                        (det (subvec (mapv (partial vec-remove i) matrix) 1)))))))
 (defn almost-inverse [matrix] (cond (= 1 (count matrix)) (/
-                                                           1 
-                                                           (first (first matrix))) 
-                                     :else (vec (for [i (range (count matrix))] 
-                                              (vec (for [j (range (count matrix))] (*
-                                                                                    (int (Math/pow -1 (+ i j))) 
-                                                                                    (det (vec-remove i (mapv (partial vec-remove j) matrix))))))))))
+                                                           1
+                                                           (first (first matrix)))
+                                    :else (vec (for [i (range (count matrix))]
+                                                 (vec (for [j (range (count matrix))] (*
+                                                                                        (int (Math/pow -1 (+ i j)))
+                                                                                        (det (vec-remove i (mapv (partial vec-remove j) matrix))))))))))
 (defn divide-vector [number vect] (mapv #(/ % number) vect))
-(defn divide-matrix [number matrix] (mapv (partial divide-vector number) matrix))                                                                                                                                                                  
+(defn divide-matrix [number matrix] (mapv (partial divide-vector number) matrix))
 (defn inverse [matrix] (let [determinant (det matrix)] (apply mapv vector ((partial divide-matrix determinant) (almost-inverse matrix)))))
